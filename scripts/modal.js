4d032f26506eef9966f38697185c2af0159ba2e6
Aslanbek <kaipaeff@gmail.com>
2023-11-19 01:03:49 +0300

    modal.js script fixed & modal for feedback added

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,39 +1,46 @@
 
 const modal = document.querySelector('.modal');
-const showModalBtn = document.querySelectorAll('.control-list__item--phone-modal');
-const closeModalBtn = document.querySelectorAll('.modal__close-btn');
-const overlay = document.querySelector('.header__modal-overlay')
-const navToggleIntoModal = document.getElementById('nav__toggle');
+const showModalBtn = document.querySelectorAll('.phone-modalBtn, .modal__close-btn, .feedback-modalBtn');
+const overlay = document.querySelector('.header__modal-overlay');
+const navToggleCheckbox = document.getElementById('nav__toggle');
 
 let isModalOpen = false;
 
+// Функция обновления стилей оверлея
+function updateOverlayStyles(isOpen) {
+  overlay.style.opacity = isOpen ? '1' : '0';
+  overlay.style.pointerEvents = isOpen ? 'visiblePainted' : 'none';
+}
+
+// Функция переключения модального окна
+function toggleModal(isOpen) {
+  modal.classList.toggle('show', isOpen);
+  updateOverlayStyles(isOpen);
+  isModalOpen = isOpen;
+  navToggleCheckbox.checked = false;
+}
+
+// Обработчик клика по кнопкам открытия модального окна
 showModalBtn.forEach((el) => {
+  const isOpenAction = el.classList.contains('phone-modalBtn') || el.classList.contains('feedback-modalBtn');
   el.addEventListener('click', (event) => {
-    modal.classList.add('show');
-    overlay.style.opacity = '1';
-    overlay.style.pointerEvents = 'visiblePainted';
-    isModalOpen = true;
-    navToggleIntoModal.checked = false;
+    toggleModal(isOpenAction);
     event.stopPropagation();
   });
-})
+});
 
-closeModalBtn.forEach((el) => {
-  el.addEventListener('click', (event) => {
-    modal.classList.remove('show');
-    overlay.style.opacity = '0';
-    overlay.style.pointerEvents = 'none';
-    isModalOpen = false;
-    navToggleIntoModal.checked = false;
-    event.stopPropagation();
-  });
-})
-
+// Обработчик клика на документе для закрытия модального окна
 document.addEventListener('click', (event) => {
   if (isModalOpen && !modal.contains(event.target)) {
-    modal.classList.remove('show');
-    overlay.style.opacity = '0';
-    overlay.style.pointerEvents = 'none';
-    event.stopPropagation();
+    toggleModal(false);
   }
 });
+
+// Обработчик нажатия клавиши Escape при открытом модальном окне для его закрытия
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && isModalOpen) {
+    event.preventDefault();
+    toggleModal(false);
+  }
+})
+
--- scripts/sidebar-links.js
+++ scripts/sidebar-links.js
@@ -5,3 +5,9 @@ const sidebarOverlay = document.getElementById('nav__sidebar-overlay');
 sidebarOverlay.addEventListener('click', () => {
   navToggle.checked = false;
 })
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    navToggle.checked = false;
+  }
+})
