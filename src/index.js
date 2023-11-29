af9f24c8b8e3187db419a801e4c425a83ae0b70e
Aslanbek <kaipaeff@gmail.com>
2023-11-29 16:17:23 +0300

    final mockup task all done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// import './index.html';
+// import './css/normalize.css';
+// import './css/style.css';
+// import './css/media.css';
+// import './css/swiper-bundle.css';
+
 import './index.html';
-import './css/normalize.css';
-import './css/style.css';
-import './css/media.css';
-import './css/swiper-bundle.css';
+import './scss/normalize.scss';
+import './scss/style.scss';
+import './scss/swiper-bundle.scss';
 
 import './scripts/modal.js';
 import './scripts/sidebar-links.js';
